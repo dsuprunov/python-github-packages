4045f78a9b225fdfddcbcd9a43e911a7ede43b24
Dmitry Suprunov <dsuprunov@gmail.com>
2023-12-13 00:43:27 +0100

    test package name

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
+
 setup(
-    name='python-github-packages',
+    name='python_github_packages',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[],
