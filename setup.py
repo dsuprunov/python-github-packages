b7278e9adf36ff1376d366dd6af487b686c2470b
Dmitry Suprunov <dsuprunov@gmail.com>
2023-12-13 00:32:02 +0100

    test package name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='corexxx',
+    name='python-core-packages',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[],
