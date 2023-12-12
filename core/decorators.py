fae95945df5cd4b497eb7b6e3faf753cd0bd0cb7
Dmitry Suprunov <dsuprunov@gmail.com>
2023-12-12 23:48:51 +0100

    initial commit

---
+++ core/decorators.py
@@ -0,0 +1,26 @@
+import time
+from functools import wraps
+
+
+def timing(func):
+    """
+    a timing decorator function
+    """
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        finish = time.perf_counter()
+        runtime = f"'{func.__name__}' took {(finish - start):.5f} second(s)"
+        print(runtime)
+
+        return result
+
+    return wrapper
+
+
+@timing
+def test():
+    time.sleep(1)
+
+test()
